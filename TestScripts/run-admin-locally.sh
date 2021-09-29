b51c3140a02b2d602d4d2816ea83c208784f68b7
RobinAxelsson <63591629+RobinAxelsson@users.noreply.github.com>
2021-09-29 07:39:57 +0200

    cleanup script added

--- TestScripts/run-admin-locally.sh
+++ TestScripts/run-admin-locally.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+
+function cleanup() {
+    unset accountEndpoint
+    unset accountKey
+    unset databaseName
+    unset containerName
+    exit
+}
+
+trap cleanup SIGINT
+
 export accountEndpoint=$(cat .secrets/accountEndpoint.secret)
 export accountKey=$(cat .secrets/accountKey.secret)
 export databaseName=$(cat .secrets/databaseName.secret)
 export containerName=$(cat .secrets/containerName.secret)
-export secretAnswer=$(cat .secrets/secretAnswer.secret)
-export flag=$(cat .secrets/flag.secret)
-cd ./src/Admin
-func start --csharp --port 7072
 
-unset accountEndpoint
-unset accountKey
-unset databaseName
-unset containerName
-unset answer
-unset flag
+cd ../src/Admin
+func start --csharp --port 7072
--- TestScripts/run-function-locally.sh
+++ TestScripts/run-function-locally.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
+
+function cleanup() {
+    unset accountEndpoint
+    unset accountKey
+    unset databaseName
+    unset containerName
+    unset answer
+    unset flag
+    exit
+}
+
+trap cleanup SIGINT
 export accountEndpoint=$(cat .secrets/accountEndpoint.secret)
 export accountKey=$(cat .secrets/accountKey.secret)
 export databaseName=$(cat .secrets/databaseName.secret)
 export containerName=$(cat .secrets/containerName.secret)
 export secretAnswer=$(cat .secrets/secretAnswer.secret)
 export flag=$(cat .secrets/flag.secret)
+
 cd ./src/Function
 func start --csharp
-
-unset accountEndpoint
-unset accountKey
-unset databaseName
-unset containerName
-unset answer
-unset flag
